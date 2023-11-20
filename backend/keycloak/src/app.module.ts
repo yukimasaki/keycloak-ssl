2cd1ccb393d1359f5e8c64be5e0a46e20a8b3a9e
yukimasaki <woodenhoe@gmail.com>
2023-11-20 13:49:50 +0900

    chore: AppModuleに.envファイル読み込み処理を追記

--- backend/keycloak/src/app.module.ts
+++ backend/keycloak/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
