d9ee1f1bb9178a026f49b88629f60323c47cf2cd
yukimasaki <woodenhoe@gmail.com>
2023-11-20 15:20:34 +0900

    feat: nest-keycloak-connect設定を追記

--- backend/keycloak/src/app.module.ts
+++ backend/keycloak/src/app.module.ts
@@ -1,13 +1,34 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard } from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot(),
+    KeycloakConnectModule.register({
+      authServerUrl: process.env.KEYCLOAK_AUTH_SERVER_URL,
+      realm: process.env.KEYCLOAK_REALM,
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      secret: process.env.KEYCLOAK_SECRET,
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule { }
