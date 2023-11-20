182e007bf2656b515d8f253fab8592a951cb90bc
yukimasaki <woodenhoe@gmail.com>
2023-11-20 15:21:39 +0900

    feat: リソース保護検証のエンドポイントを作成

--- backend/keycloak/src/user/user.controller.ts
+++ backend/keycloak/src/user/user.controller.ts
@@ -2,10 +2,44 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles, Unprotected } from 'nest-keycloak-connect';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
+
+  private HELLO: string = 'Hello NestJS';
+
+  @Get('/public')
+  @Unprotected()
+  getPublic(): string {
+    return `${this.HELLO} from public`;
+  }
+
+  @Get('/user')
+  @Roles({
+    roles: [
+      'user',
+    ],
+  })
+  getUser(): string {
+    return `${this.HELLO} from user`;
+  }
+
+  @Get('/admin')
+  @Roles({
+    roles: [
+      'admin',
+    ],
+  })
+  getAdmin(): string {
+    return `${this.HELLO} from admin`;
+  }
+
+  @Get('/all')
+  getAll(): string {
+    return `${this.HELLO} from all`;
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
