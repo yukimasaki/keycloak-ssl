62bf65af9f76d1b851f84e9ac945177fe28407ef
yukimasaki <woodenhoe@gmail.com>
2023-11-29 14:15:37 +0000

    feat: ProfileEntityを作成

--- backend/keycloak/src/profiles/entities/profile.entity.ts
+++ backend/keycloak/src/profiles/entities/profile.entity.ts
@@ -1,1 +1,21 @@
-export class Profile {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class Profile {
+  @ApiProperty({
+    example: '1',
+    description: 'プロフィールID',
+  })
+  id: number;
+
+  @ApiProperty({
+    example: '26b0a969-028d-4955-8c14-5e3056b6c099',
+    description: 'UUID',
+  })
+  uuid: string;
+
+  @ApiProperty({
+    example: 'John Doe',
+    description: '表示名',
+  })
+  userName: string;
+}
