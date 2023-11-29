3bccddc95c51fcd3422cb3aea7bfa0e1c909f59e
yukimasaki <woodenhoe@gmail.com>
2023-11-29 13:57:09 +0000

    feat: npx nest g resource profiles

--- backend/keycloak/src/app.module.ts
+++ backend/keycloak/src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard } from 'nest
 import { APP_GUARD } from '@nestjs/core';
 import { ProfileModule } from './profile/profile.module';
 import { PrismaService } from './common/prisma/prisma.service';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { PrismaService } from './common/prisma/prisma.service';
       secret: process.env.KEYCLOAK_SECRET,
     }),
     ProfileModule,
+    ProfilesModule,
   ],
   controllers: [],
   providers: [
