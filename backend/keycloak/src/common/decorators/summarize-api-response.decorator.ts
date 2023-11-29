f93082b18544caa49e6b9b1fe9fb1d46451b28fb
yukimasaki <woodenhoe@gmail.com>
2023-11-29 14:21:27 +0000

    feat: SummarizeApiResponseデコレータを作成

---
+++ backend/keycloak/src/common/decorators/summarize-api-response.decorator.ts
@@ -0,0 +1,36 @@
+// 標準レスポンスのカスタムデコレータ
+
+import { applyDecorators } from '@nestjs/common'
+import {
+  ApiBadRequestResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiUnauthorizedResponse,
+  ApiInternalServerErrorResponse
+} from '@nestjs/swagger'
+import { ErrorResponse } from '../interfaces/error-response.intaerface'
+
+export function SummarizeApiResponse() {
+  return applyDecorators(
+    ApiBadRequestResponse({
+      type: ErrorResponse,
+      description: 'BadRequest.'
+    }),
+    ApiForbiddenResponse({
+      type: ErrorResponse,
+      description: 'Forbidden.'
+    }),
+    ApiUnauthorizedResponse({
+      type: ErrorResponse,
+      description: 'Unauthorized.'
+    }),
+    ApiNotFoundResponse({
+      type: ErrorResponse,
+      description: 'Not Found.'
+    }),
+    ApiInternalServerErrorResponse({
+      type: ErrorResponse,
+      description: 'Internal Server Error.'
+    })
+  )
+}
