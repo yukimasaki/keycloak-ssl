7bfeadcf92f8611f2e59c095d4ac519c1f5e22e1
yukimasaki <woodenhoe@gmail.com>
2023-11-29 13:45:43 +0000

    feat PrismaServiceをコンストラクタに追記

--- backend/keycloak/src/profile/profile.service.ts
+++ backend/keycloak/src/profile/profile.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class ProfileService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) { }
+
   create(createProfileDto: CreateProfileDto) {
     return 'This action adds a new profile';
   }
