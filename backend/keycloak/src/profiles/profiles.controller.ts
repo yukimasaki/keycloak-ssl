b8a1e39bced6259fb8f62d3b31a1cee74707cdc5
yukimasaki <woodenhoe@gmail.com>
2023-11-29 14:16:20 +0000

    feat: GET /profiles/:uuidにSwaggerデコレータを追記

--- backend/keycloak/src/profiles/profiles.controller.ts
+++ backend/keycloak/src/profiles/profiles.controller.ts
@@ -3,8 +3,11 @@ import { ProfilesService } from './profiles.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { Public } from 'nest-keycloak-connect';
+import { ApiProduces, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Profile } from './entities/profile.entity';
 
 @Controller('profiles')
+@ApiTags('/profiles')
 export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) { }
 
@@ -20,7 +23,21 @@ export class ProfilesController {
 
   @Get(':uuid')
   @Public()
-  findOne(@Param('uuid') uuid: string) {
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: '単体取得API' })
+  @ApiParam({
+    name: 'uuid',
+    type: String,
+    example: '26b0a969-028d-4955-8c14-5e3056b6c099',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '指定されたUUIDのプロフィール情報を返却',
+    type: Profile,
+  })
+  findOne(
+    @Param('uuid') uuid: string,
+  ) {
     return this.profilesService.findOne(uuid);
   }
 
