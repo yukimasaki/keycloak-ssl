414b9e34c1197711dac9733e96ae8858edbc0c88
yukimasaki <woodenhoe@gmail.com>
2023-11-30 05:32:25 +0000

    feat: JwtServiceを注入

--- backend/keycloak/src/profiles/profiles.module.ts
+++ backend/keycloak/src/profiles/profiles.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { ProfilesController } from './profiles.controller';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [ProfilesController],
   providers: [
     ProfilesService,
     PrismaService,
+    JwtService,
   ],
 })
 export class ProfilesModule { }
