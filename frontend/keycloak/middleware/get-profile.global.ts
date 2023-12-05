94851dd43f87ca07d504c5fde5c82a94b9ad3e56
yukimasaki <woodenhoe@gmail.com>
2023-12-05 08:16:06 +0000

    feat: Keycloakインスタンスからtokenを取得することに成功

--- frontend/keycloak/middleware/get-profile.global.ts
+++ frontend/keycloak/middleware/get-profile.global.ts
@@ -3,17 +3,12 @@ export default defineNuxtRouteMiddleware(async () => {
   const apiUrl: string = runtimeConfig.public.apiUrl;
   if (!apiUrl) throw new Error('API URLが設定されていません');
 
-  const keycloak: any = useNuxtApp().$keycloak;
-  console.log(`keycloak:`);
-  console.log(keycloak);
-  // undefinedになってしまう
-  console.log(`keycloak.token:`);
-  console.log(keycloak.token);
+  const token: string | undefined = useNuxtApp().$keycloak.token;
 
   // データベースからプロフィール情報を取得
   const { data: profile } = await useFetch(`${apiUrl}/profiles/me`, {
     headers: {
-      Authorization: `Bearer ${keycloak.token}`,
+      Authorization: `Bearer ${token}`,
     },
   });
 
