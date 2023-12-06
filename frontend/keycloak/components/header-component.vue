a3599b0c40425e3dd7f27fcf393ef5b0716938b1
yukimasaki <woodenhoe@gmail.com>
2023-12-06 01:34:07 +0000

    feat: HeaderComponent作成

---
+++ frontend/keycloak/components/header-component.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-header elevated class="q-pa-sm bg-light-blue-6">
+    <q-toolbar-title @click="navigateToIndex()">
+      Keycloak サンプル
+    </q-toolbar-title>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+const navigateToIndex = () => {
+  return navigateTo('/');
+}
+</script>
