ef7e1faaeb0ab2dff78b0fd56b7c321f655e3bbe
yukimasaki <woodenhoe@gmail.com>
2023-12-07 03:00:07 +0000

    feat: 返却された配列が空の場合NotFoundExceptionをスロー

--- backend/keycloak/src/books/books.service.ts
+++ backend/keycloak/src/books/books.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { Book } from './entities/book.entity';
 
 @Injectable()
 export class BooksService {
@@ -7,6 +8,8 @@ export class BooksService {
     private readonly prisma: PrismaService,
   ) { }
   async findAll() {
-    return await this.prisma.book.findMany();
+    const books: Book[] = await this.prisma.book.findMany()
+    if (books.length === 0) throw new NotFoundException;
+    return books;
   }
 }
