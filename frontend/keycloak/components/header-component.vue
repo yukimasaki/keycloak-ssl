d87d8606fde4664b26e455110382d00ddf4a006d
yukimasaki <woodenhoe@gmail.com>
2023-12-07 08:18:14 +0000

    feat: tokenが存在しない場合はログアウトボタンを非表示

--- frontend/keycloak/components/header-component.vue
+++ frontend/keycloak/components/header-component.vue
@@ -2,7 +2,7 @@
   <q-header elevated class="q-pa-sm bg-light-blue-6">
     <q-toolbar>
       <q-toolbar-title @click="navigateToIndex()">Keycloak サンプル</q-toolbar-title>
-      <q-btn flat dense icon="logout" @click="logout()">Logout</q-btn>
+      <q-btn flat dense icon="logout" @click="logout()" v-if="token">Logout</q-btn>
     </q-toolbar>
   </q-header>
 </template>
@@ -15,4 +15,6 @@ const navigateToIndex = () => {
 const logout = async () => {
   return await useNuxtApp().$keycloak.logout();
 }
+
+const token: string | undefined = useNuxtApp().$keycloak.token;
 </script>
