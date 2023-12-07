70e66934546d9b21bbcc035ece00cc68395e91ec
yukimasaki <woodenhoe@gmail.com>
2023-12-07 02:30:42 +0000

    feat: BooksController作成中

--- backend/keycloak/src/books/books.controller.ts
+++ backend/keycloak/src/books/books.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { ApiOperation, ApiProduces, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SummarizeApiResponse } from 'src/common/decorators/summarize-api-response.decorator';
+import { Public } from 'nest-keycloak-connect';
+import { Book } from './entities/book.entity';
 
 @Controller('books')
+@ApiTags('/books')
+@SummarizeApiResponse()
 export class BooksController {
-  constructor(private readonly booksService: BooksService) {}
+  constructor(private readonly booksService: BooksService) { }
+
+  @Get('/public')
+  @Public()
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: '単体取得API (パブリックアクセス)' })
+  @ApiResponse({
+    status: 200,
+    description: '全ての書籍情報を返却する',
+    type: Book,
+  })
+  findAll() { }
 }
