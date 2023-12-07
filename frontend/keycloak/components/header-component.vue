1cae20fbffe1e6e8c26980e7e5b01e80a864ed0f
yukimasaki <woodenhoe@gmail.com>
2023-12-07 06:47:29 +0000

    feat: ヘッダーにログアウトボタンを配置

--- frontend/keycloak/components/header-component.vue
+++ frontend/keycloak/components/header-component.vue
@@ -1,13 +1,18 @@
 <template>
   <q-header elevated class="q-pa-sm bg-light-blue-6">
-    <q-toolbar-title @click="navigateToIndex()">
-      Keycloak サンプル
-    </q-toolbar-title>
+    <q-toolbar>
+      <q-toolbar-title @click="navigateToIndex()">Keycloak サンプル</q-toolbar-title>
+      <q-btn flat dense icon="logout" @click="logout()">Logout</q-btn>
+    </q-toolbar>
   </q-header>
 </template>
 
 <script setup lang="ts">
 const navigateToIndex = () => {
   return navigateTo('/');
+}
+
+const logout = async () => {
+  return await useNuxtApp().$keycloak.logout();
 }
 </script>
