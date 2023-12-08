a87194490749b727c6c1c509c8118246acda12c7
yukimasaki <woodenhoe@gmail.com>
2023-12-08 01:57:36 +0000

    feat: /profileへ強制遷移する処理を一番上に移動

--- frontend/keycloak/middleware/get-profile.global.ts
+++ frontend/keycloak/middleware/get-profile.global.ts
@@ -1,9 +1,19 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  // /profileに強制遷移するのを除外するページ
+  const excludes: string[] = [
+    '/test',
+    '/welcome',
+  ];
+  if (excludes.includes(to.path)) return;
+
   const runtimeConfig = useRuntimeConfig();
   const apiUrl: string = runtimeConfig.public.apiUrl;
   if (!apiUrl) throw new Error('API URLが設定されていません');
 
   const token: string | undefined = useNuxtApp().$keycloak.token;
+
+  // アクセストークンが存在しない場合はログインが必要
+  if (!token) useNuxtApp().$keycloak.login();
 
   // データベースからプロフィール情報を取得
   const { data: profile } = await useFetch(`${apiUrl}/profiles/me`, {
@@ -12,17 +22,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     },
   });
 
-  // /profileに強制遷移するのを除外するページ
-  const excludes: string[] = [
-    '/test',
-    '/welcome',
-  ];
-  if (excludes.includes(to.path)) return;
-
-  // アクセストークンが存在しない場合はログインが必要
-  if (!token) useNuxtApp().$keycloak.login();
-
-  // DBにプロフィール情報が存在しない場合はプロフィールの設定が必要
   if (!profile.value && to.path !== '/profile') return navigateTo('/profile');
 
   useState('profile', () => profile);
