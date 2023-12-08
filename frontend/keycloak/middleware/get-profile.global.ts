d357fe73f3619bb12a6064eaaa0638983612e979
yukimasaki <woodenhoe@gmail.com>
2023-12-08 04:09:23 +0000

    feat: check-ssoモードだとiPhoneでログインできないため、login-requiredに戻した

--- frontend/keycloak/middleware/get-profile.global.ts
+++ frontend/keycloak/middleware/get-profile.global.ts
@@ -1,19 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  // /profileに強制遷移するのを除外するページ
-  const excludes: string[] = [
-    '/test',
-    '/welcome',
-  ];
-  if (excludes.includes(to.path)) return;
-
   const runtimeConfig = useRuntimeConfig();
   const apiUrl: string = runtimeConfig.public.apiUrl;
   if (!apiUrl) throw new Error('API URLが設定されていません');
 
   const token: string | undefined = useNuxtApp().$keycloak.token;
-
-  // アクセストークンが存在しない場合はログインが必要
-  if (!token) useNuxtApp().$keycloak.login();
 
   // データベースからプロフィール情報を取得
   const { data: profile } = await useFetch(`${apiUrl}/profiles/me`, {
--- frontend/keycloak/plugins/keycloak.client.ts
+++ frontend/keycloak/plugins/keycloak.client.ts
@@ -9,7 +9,7 @@ export default defineNuxtPlugin(async (app) => {
   });
 
   await keycloak.init({
-    onLoad: "check-sso",
+    onLoad: "login-required",
   });
 
   keycloak.updateToken(2000);
