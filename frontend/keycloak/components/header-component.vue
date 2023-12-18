1ea270915b48b2c4407fe19040452a2b1a2cfa01
yukimasaki <woodenhoe@gmail.com>
2023-12-18 10:13:57 +0000

    feat: Keycloak関連処理をコメントアウト

--- frontend/keycloak/components/header-component.vue
+++ frontend/keycloak/components/header-component.vue
@@ -2,26 +2,33 @@
   <q-header elevated class="q-pa-sm bg-light-blue-6">
     <q-toolbar>
       <q-toolbar-title @click="navigateToIndex()">Keycloak サンプル</q-toolbar-title>
-      <q-btn flat dense icon="login" @click="login()" v-if="!token">Login</q-btn>
-      <q-btn flat dense icon="logout" @click="logout()" v-if="token">Logout</q-btn>
+      <q-btn flat dense icon="login" @click="login()">Login</q-btn>
+      <!-- <q-btn flat dense icon="login" @click="login()" v-if="!token">Login</q-btn> -->
+      <!-- <q-btn flat dense icon="logout" @click="logout()" v-if="token">Logout</q-btn> -->
     </q-toolbar>
   </q-header>
 </template>
 
 <script setup lang="ts">
+const { signIn } = useAuth();
+
 const navigateToIndex = () => {
   return navigateTo('/');
 }
 
 const login = async () => {
-  return await useNuxtApp().$keycloak.login({
-    redirectUri: window.origin,
-  });
+  return await signIn('');
 }
 
-const logout = async () => {
-  return await useNuxtApp().$keycloak.logout();
-}
+// const login = async () => {
+//   return await useNuxtApp().$keycloak.login({
+//     redirectUri: window.origin,
+//   });
+// }
 
-const token: string | undefined = useNuxtApp().$keycloak.token;
+// const logout = async () => {
+//   return await useNuxtApp().$keycloak.logout();
+// }
+
+// const token: string | undefined = useNuxtApp().$keycloak.token;
 </script>
