30164cec4da1d752783eb1a0900e06d94c47b090
yukimasaki <woodenhoe@gmail.com>
2023-12-20 07:38:17 +0000

    feat: ログイン状態に応じてログイン・ログアウトボタンの表示を切り替え

--- frontend/keycloak/components/header-component.vue
+++ frontend/keycloak/components/header-component.vue
@@ -2,15 +2,14 @@
   <q-header elevated class="q-pa-sm bg-light-blue-6">
     <q-toolbar>
       <q-toolbar-title @click="navigateToIndex()">Keycloak サンプル</q-toolbar-title>
-      <q-btn flat dense icon="login" @click="login()">Login</q-btn>
-      <!-- <q-btn flat dense icon="login" @click="login()" v-if="!token">Login</q-btn> -->
-      <!-- <q-btn flat dense icon="logout" @click="logout()" v-if="token">Logout</q-btn> -->
+      <q-btn flat dense icon="login" @click="login()" v-if="status === 'unauthenticated'">Login</q-btn>
+      <q-btn flat dense icon="logout" @click="logout()" v-if="status === 'authenticated'">Logout</q-btn>
     </q-toolbar>
   </q-header>
 </template>
 
 <script setup lang="ts">
-const { signIn } = useAuth();
+const { signIn, signOut, status } = useAuth();
 
 const navigateToIndex = () => {
   return navigateTo('/');
@@ -20,15 +19,8 @@ const login = async () => {
   return await signIn('keycloak');
 }
 
-// const login = async () => {
-//   return await useNuxtApp().$keycloak.login({
-//     redirectUri: window.origin,
-//   });
-// }
+const logout = async () => {
+  return await signOut();
+}
 
-// const logout = async () => {
-//   return await useNuxtApp().$keycloak.logout();
-// }
-
-// const token: string | undefined = useNuxtApp().$keycloak.token;
 </script>
