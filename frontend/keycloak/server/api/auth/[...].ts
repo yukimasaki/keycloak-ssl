835c428ab885de81906d9ab4eb7a799bd33d066c
yukimasaki <woodenhoe@gmail.com>
2023-12-20 02:27:38 +0000

    feat: KeycloakProviderを設定

--- frontend/keycloak/server/api/auth/[...].ts
+++ frontend/keycloak/server/api/auth/[...].ts
@@ -1,58 +1,12 @@
 import { NuxtAuthHandler } from '#auth';
-import { Session } from 'next-auth';
-import { JWT } from 'next-auth/jwt';
-import { Provider } from 'next-auth/providers';
-
-interface ExtendedSession extends Session {
-  accessToken?: string;
-}
-
-interface ExtendedJwt extends JWT {
-  accessToken?: string;
-}
-
-const keycloak: Provider = {
-  id: 'keycloak',
-  name: 'Keycloak Public Client',
-  wellKnown: process.env.NUXT_PUBLIC_AUTH_WELLKNOWN,
-  type: 'oauth',
-  authorization: {
-    params: {
-      scope: 'openid email profile',
-    },
-  },
-  idToken: true,
-  clientId: process.env.NUXT_PUBLIC_AUTH_CLIENT_ID,
-  client: {
-    token_endpoint_auth_method: 'none',
-  },
-  profile(profile) {
-    return {
-      id: profile.sid,
-      name: profile.preferred_username,
-      email: profile.email,
-      image: 'none',
-    }
-  },
-}
+import KeycloakProvider from "next-auth/providers/keycloak";
 
 export default NuxtAuthHandler({
-  secret: 'secret',
   providers: [
-    keycloak,
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_ID || '',
+      clientSecret: process.env.KEYCLOAK_SECRET || '',
+      issuer: process.env.KEYCLOAK_ISSUER || '',
+    }),
   ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session(
-      { session, token }: { session: ExtendedSession, token: ExtendedJwt }
-    ) {
-      session.accessToken = token.accessToken;
-      return session;
-    }
-  },
 });
