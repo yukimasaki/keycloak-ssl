d8131c5e16e18eca29ee01447283569bb8caa3d7
yukimasaki <woodenhoe@gmail.com>
2023-12-21 06:32:05 +0000

    feat: server/middlewareを削除

--- frontend/keycloak/server/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from '#auth'
-import { navigateTo } from 'nuxt/app';
-
-export default eventHandler(async (event: any) => {
-  const session = await getServerSession(event);
-  if (!session) {
-    // return await navigateTo('/api/auth');
-  }
-});
