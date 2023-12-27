7cff0e6b41ac56271d971791e7c1cad03726c912
yukimasaki <woodenhoe@gmail.com>
2023-12-27 04:44:51 +0000

    chore: PM2のログファイルに日時を記録する対応

--- frontend/keycloak/ecosystem.config.cjs
+++ frontend/keycloak/ecosystem.config.cjs
@@ -6,6 +6,8 @@ module.exports = {
       instances: '-1',
       script: '.output/server/index.mjs',
       args: 'start',
+      time: true,
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
       env: {
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         AUTH_ORIGIN: process.env.AUTH_ORIGIN,
