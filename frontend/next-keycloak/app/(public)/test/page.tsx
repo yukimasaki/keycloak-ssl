ef58d2d7ec1b390def39d53d49d9d83d99d5f121
yukimasaki <woodenhoe@gmail.com>
2023-12-28 02:39:48 +0000

    feat: TestPage作成

---
+++ frontend/next-keycloak/app/(public)/test/page.tsx
@@ -0,0 +1,12 @@
+import { HeaderComponent } from "@components/header";
+
+export default function TestPage() {
+  return (
+    <>
+      <HeaderComponent />
+      <p>
+        Test Page
+      </p>
+    </>
+  );
+}
