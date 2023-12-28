2d2e3f5d565987b269f35405628c84c38be8a438
yukimasaki <woodenhoe@gmail.com>
2023-12-28 01:06:26 +0000

    feat: 環境変数を記載

--- frontend/next-keycloak/next.config.js
+++ frontend/next-keycloak/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverRuntimeConfig: {
+    keycloakClientId: process.env.KEYCLOAK_CLIENT_ID,
+    keycloakClientSecret: process.env.KEYCLOAK_CLIENT_ID,
+    keycloakIssuer: process.env.KEYCLOAK_CLIENT_ID,
+    nextAuthUrl: process.env.NEXTAUTH_URL,
+  },
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
