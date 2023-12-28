b548a52814dee99058d5f1da57490a39917d203f
yukimasaki <woodenhoe@gmail.com>
2023-12-28 06:07:32 +0000

    feat: ログイン・ログアウトボタン作成

--- frontend/next-keycloak/app/_components/header.tsx
+++ frontend/next-keycloak/app/_components/header.tsx
@@ -1,19 +1,24 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+import { LoginButtonComponent } from "@components/loginButton";
+import { LogoutButtonComponent } from "@components/logoutButton";
+import { useSession } from "next-auth/react";
 
 export const HeaderComponent = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
+
   return (
-    <Navbar>
+    <Navbar className="shadow">
       <NavbarBrand>
-        <p className="font-bold text-inherit">Next Keycloak</p>
+        <NavbarItem>
+          <p className="font-bold text-inherit">
+            <Link href="/">Next Keycloak</Link>
+          </p>
+        </NavbarItem>
       </NavbarBrand>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          {!user ? <LoginButtonComponent /> : <LogoutButtonComponent />}
         </NavbarItem>
       </NavbarContent>
     </Navbar>
---
+++ frontend/next-keycloak/app/_components/logoutButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+
+export const LogoutButtonComponent = () => {
+  return (
+    <Button as={Link} color="primary" href="#" onClick={() => signOut()} variant="flat">
+      Logout
+    </Button>
+  );
+}
