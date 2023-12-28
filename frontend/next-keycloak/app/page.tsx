2a15665d000c627519001dd4e00e7900a903eaf1
yukimasaki <woodenhoe@gmail.com>
2023-12-28 06:10:13 +0000

    feat: HomePageのリストをmap関数でレンダリング

--- frontend/next-keycloak/app/page.tsx
+++ frontend/next-keycloak/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { HeaderComponent } from "@components/header";
 import { ListboxWrapperComponent } from "@components/listBoxWrapper";
@@ -6,25 +6,38 @@ import { Listbox, ListboxItem } from "@nextui-org/react";
 import { GroupsIcon } from "@components/icons/groups";
 
 export default function HomePage() {
-  const iconClasses = "text-xl text-default-500 pointer-events-none flex-shrink-0";
+  const iconClasses = "text-xl text-blue-500 pointer-events-none flex-shrink-0";
+
+  const pages = [
+    {
+      name: "Home",
+      path: "/",
+      icon: GroupsIcon({ className: iconClasses }),
+    },
+    {
+      name: "Test",
+      path: "/test",
+      icon: GroupsIcon({ className: iconClasses }),
+    },
+  ];
+
   return (
     <>
       <HeaderComponent />
       <div className="flex items-center justify-center p-4">
         <ListboxWrapperComponent>
           <Listbox>
-            <ListboxItem
-              key="HomePage"
-              startContent={<GroupsIcon className={iconClasses} />}
-            >
-              Home
-            </ListboxItem>
-            <ListboxItem
-              key="TestPage"
-              startContent={<GroupsIcon className={iconClasses} />}
-            >
-              Test
-            </ListboxItem>
+            {pages.map(page => {
+              return (
+                <ListboxItem
+                  key={page.name}
+                  startContent={page.icon}
+                  href={page.path}
+                >
+                  {page.name}
+                </ListboxItem>
+              );
+            })}
           </Listbox>
         </ListboxWrapperComponent>
       </div>
