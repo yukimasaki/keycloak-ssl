1f8ff95ae3cb30ab55acc4f4598eb2a82d062bb3
yukimasaki <woodenhoe@gmail.com>
2023-12-28 04:29:11 +0000

    feat: HomePageにリスト要素を表示

--- frontend/next-keycloak/app/page.tsx
+++ frontend/next-keycloak/app/page.tsx
@@ -1,7 +1,33 @@
+"use client"
+
 import { HeaderComponent } from "@components/header";
+import { ListboxWrapperComponent } from "@components/listBoxWrapper";
+import { Listbox, ListboxItem } from "@nextui-org/react";
+import { GroupsIcon } from "@components/icons/groups";
 
 export default function HomePage() {
+  const iconClasses = "text-xl text-default-500 pointer-events-none flex-shrink-0";
   return (
-    <HeaderComponent />
+    <>
+      <HeaderComponent />
+      <div className="flex items-center justify-center p-4">
+        <ListboxWrapperComponent>
+          <Listbox>
+            <ListboxItem
+              key="HomePage"
+              startContent={<GroupsIcon className={iconClasses} />}
+            >
+              Home
+            </ListboxItem>
+            <ListboxItem
+              key="TestPage"
+              startContent={<GroupsIcon className={iconClasses} />}
+            >
+              Test
+            </ListboxItem>
+          </Listbox>
+        </ListboxWrapperComponent>
+      </div>
+    </>
   );
 }
