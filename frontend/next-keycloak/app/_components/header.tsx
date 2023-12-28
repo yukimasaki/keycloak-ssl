a77de6463c151289fa776ee9869e13984c165b3c
yukimasaki <woodenhoe@gmail.com>
2023-12-28 04:28:42 +0000

    feat: ヘッダー中央の要素を削除

--- frontend/next-keycloak/app/_components/header.tsx
+++ frontend/next-keycloak/app/_components/header.tsx
@@ -6,23 +6,6 @@ export const HeaderComponent = () => {
       <NavbarBrand>
         <p className="font-bold text-inherit">Next Keycloak</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
